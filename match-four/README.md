ec625348d34335f7a51601ab6e92147f7c3a2412
borntofrappe <borntofrappe@gmail.com>
2020-03-02 23:56:32 +0100

    add first grid in the terminal

--- match-four/README.md
+++ match-four/README.md
@@ -4,4 +4,8 @@ A game in which you are tasked to match shapes of the same color.
 
 ## Notes
 
-This project is inspired by [this tutorial]() on [freecodecamp YouTube channel]() and it is actually scheduled in two variants. The first variant uses the console, the terminal to play the game, while the second one replicates the logic with more visuals in pygame.
+This project is inspired by [this tutorial]() on [freecodecamp YouTube channel]() and it is actually scheduled to produce two games.
+
+## Terminal
+
+Using ASCII characters, the idea is to show a grid of `n` columns and , filled with empty spaces. Select a column typing the matching integer, and have a space filled with the selected color.
--- match-four/script.py
+++ match-four/script.py
@@ -1,3 +1,58 @@
-"""
-match four, in the console
-"""
+class Grid:
+    def __str__(self):
+        grid = self.grid
+        grid_transposed = [[row[i] for row in grid] for i in range(len(grid))]
+        output = ""
+        for row in grid_transposed:
+            output += "|"
+            for cell in row:
+                output = output + str(cell).center(3, " ") + "|"
+            output += "\n"
+
+        output += " "
+        for x in range(self.columns):
+            output += str(x).center(4, " ")
+        return output
+
+    def make_grid(self, columns, rows):
+        return [[" " for x in range(columns)] for y in range(rows)]
+
+    def __init__(self, columns, rows):
+        self.columns = columns
+        self.rows = rows
+        self.grid = self.make_grid(self.columns, self.rows)
+
+    def empty_grid(self):
+        self.grid = self.make_grid(self.columns, self.rows)
+
+    def select_column(self, x, player="R"):
+        column = self.grid[x]
+        try:
+            index = list(reversed(column)).index(" ")
+            column[len(column) - index - 1] = player
+        except ValueError:
+            print("Not found")
+
+
+columns = 5
+rows = 5
+grid = Grid(columns, rows)
+
+players = ["R", "Y"]
+are_playing = True
+player = "R"
+while(are_playing):
+    print(grid)
+    print()
+    print("Player: " + player)
+    selection = input("Select column: ").rstrip()
+    if(selection == "q"):
+        are_playing = False
+        break
+    print()
+    grid.select_column(int(selection), player)
+
+    if player == "R":
+        player = "Y"
+    else:
+        player = "R"
