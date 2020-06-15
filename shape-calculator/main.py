c2549a8df3ea66b38b048638d1c08ffb3d78f986
borntofrappe <borntofrappe@gmail.com>
2020-06-15 18:13:30 +0200

    add working code

--- shape-calculator/main.py
+++ shape-calculator/main.py
@@ -1,13 +1,18 @@
-import shape_calculator_previous as shape_calculator
+import shape_calculator
 
 rect = shape_calculator.Rectangle(5, 10)
 print(rect.get_area())
 rect.set_width(3)
 print(rect.get_perimeter())
 print(rect)
+print(rect.get_picture())
 
 sq = shape_calculator.Square(9)
 print(sq.get_area())
 sq.set_side(4)
 print(sq.get_diagonal())
 print(sq)
+print(sq.get_picture())
+
+sq_inside = shape_calculator.Square(2)
+print(sq.get_amount_inside(sq_inside))
---
+++ shape-calculator/shape_calculator.py
@@ -0,0 +1,64 @@
+class Rectangle:
+    def __str__(self):
+        w = str(self.width)
+        h = str(self.height)
+        return 'Rectangle(width=' + w + ', height=' + h + ')'
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+
+        output = ""
+        for row in range(self.height):
+            output += "*" * self.width
+            output += "\n"
+        return output
+
+    def get_amount_inside(self, shape):
+        width_inside = self.width // shape.width
+        height_inside = self.height // shape.height
+        return width_inside * height_inside
+
+
+class Square(Rectangle):
+    def __str__(self):
+        # additional check to guarantee that the width and height match
+        w = str(self.width)
+        h = str(self.height)
+        if w == h:
+            return 'Square(side=' + w + ')'
+        else:
+            return 'Mis-shaped Square(width=' + w + ',height=' + h + ')'
+
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    # modify the width and height setters to guarantee the two measures are updated together
+    def set_width(self, width):
+        self.set_side(width)
+
+    def set_height(self, height):
+        self.set_side(height)
