ee521f776474eed3acf28a033d87361561e95839
borntofrappe <borntofrappe@gmail.com>
2020-06-18 23:39:23 +0200

    add working code

--- numpy-stats/main.py
+++ numpy-stats/main.py
@@ -1,3 +1,3 @@
-import numpy_stats_previous as numpy_stats
+import numpy_stats
 
-print(numpy_stats.calculate([0,1,2,3,4,5,6,7,8]))
+print(numpy_stats.calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
---
+++ numpy-stats/numpy_stats.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def calculate(list):
+    array = np.array(list)
+
+    try:
+        matrix = array.reshape((3, 3))
+    except ValueError:
+        raise ValueError("List must contain nine numbers.")
+    else:
+        mean = [np.mean(matrix, axis=0).tolist(), np.mean(
+            matrix, axis=1).tolist(), np.mean(array).tolist()]
+
+        variance = [np.var(matrix, axis=0).tolist(), np.var(
+            matrix, axis=1).tolist(), np.var(array).tolist()]
+
+        standard_deviation = [np.std(matrix, axis=0).tolist(), np.std(
+            matrix, axis=1).tolist(), np.std(array).tolist()]
+
+        max = [np.max(matrix, axis=0).tolist(), np.max(
+            matrix, axis=1).tolist(), np.max(array).tolist()]
+        min = [np.min(matrix, axis=0).tolist(), np.min(
+            matrix, axis=1).tolist(), np.min(array).tolist()]
+        sum = [np.sum(matrix, axis=0).tolist(), np.sum(
+            matrix, axis=1).tolist(), np.sum(array).tolist()]
+
+        calculations = {
+            "mean": mean,
+            "variance": variance,
+            "standard deviation": standard_deviation,
+            "max": max,
+            "min": min,
+            "sum": sum
+        }
+
+        return calculations
